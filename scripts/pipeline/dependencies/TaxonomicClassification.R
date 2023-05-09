bf45332e4f1052f162d5d6f057a07a272158e5dd
Mathias Verbeke <mathias.verbeke@student.howest.be>
2023-05-09 17:09:52 +0200

    Removed reference database validation

--- scripts/pipeline/dependencies/TaxonomicClassification.R
+++ scripts/pipeline/dependencies/TaxonomicClassification.R
@@ -12,10 +12,6 @@ ref_files <- ref_files[!basename(ref_files) == 'config.txt']
 # Read in the csv file
 config_df <- read.csv(config_file)
 
-# Check if the names of the reference databases match with those in the config file
-config_ref <- config_df[,'Database']
-print(config_ref)
-
 # Read in the input multifasta file
 seqs <- readDNAStringSet(path.ASV)
 IDs <- names(seqs)
@@ -32,9 +28,9 @@ for (ref_file in ref_files) {
   
   cat(paste('Reference database:', ref_name, '\n'))
   
-  if(!dir.exists(normalizePath(file.path(path.taxon, ref_name)))){
-    cat(paste('Creating output directories:', normalizePath(file.path(path.taxon, ref_name), '\n')))
-    dir.create(normalizePath(file.path(path.taxon, ref_name)))
+  if(!dir.exists(file.path(path.taxon, ref_name))){
+    cat(paste('Creating output directories:', file.path(path.taxon, ref_name), '\n'))
+    dir.create(file.path(path.taxon, ref_name))
   }
   
   # Create the taxonomy table using the DADA2 assignTaxonomy function
@@ -42,5 +38,5 @@ for (ref_file in ref_files) {
   tax_table <- cbind(ID = IDs, tax_table)
   
   # Print the taxonomy table to an Excel file
-  write.xlsx(tax_table, file = normalizePath(file.path(path.taxon, ref_name, paste0(ref_name,'_tax_classification', '.xlsx'))))
+  write.xlsx(tax_table, file = file.path(path.taxon, ref_name, paste0(ref_name,'_tax_classification', '.xlsx')))
 }
