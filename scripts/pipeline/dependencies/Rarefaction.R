103b985f932d919c5747a6af246fca57fcc163f9
Mathias Verbeke <mathias.verbeke@student.howest.be>
2023-05-15 14:39:09 +0200

    Added rarefaction script for ASVs

--- scripts/pipeline/dependencies/Rarefaction.R
+++ scripts/pipeline/dependencies/Rarefaction.R
@@ -1,32 +1,31 @@
 library(vegan)
 
-data <- readRDS(file = '/home/guest/Internship/data/output/RUN2/06.Seq_Table/seqtab.rds')
+# Read in the data (this is not yet species data)
+data <- readRDS(file = '/home/guest/Traineeship/ARMSProject/data/temp2/ARMS_SWC_Gbg2_20200206_20200520/06.Seq_Table/seqtab.rds')
 
-rowSums(data)
+# Remove rows where the sum of the observed count data across entire row is smaller than 2 
+data <- data[!rowSums(data) <= 1,]
 
-S <- specnumber(data[c(1:2, 4:5),]) # observed number of species
-(raremax <- min(rowSums(data[c(1:2, 4:5),])))
-Srare <- rarefy(data[c(1:2, 4:5),], raremax)
-plot(S, Srare, xlab = "Observed No. of Species", ylab = "Rarefied No. of Species")
-abline(0, 1)
-rarecurve(data[c(1:2, 4:5),], step = 20, sample = raremax, col = "blue", cex = 0.6)
+S <- specnumber(data) # observed number of ASV sequences, same as apply(data, 1, function(x){sum(x != 0)})
 
-rowSums(data[])
+(raremax <- min(rowSums(data))) # rarefaction threshold
 
 
-# Examle code
-install.packages('vegan')
+# The rarefy() function is used to estimate the expected number of species 
+# (or ASV sequences) in random subsamples of a specified size from a community 
+# or dataset. It is a common method for comparing species richness or diversity 
+# across different samples or communities. In the line below, rarefy(data, raremax) 
+# calculates the rarefied species richness by simulating the expected number of 
+# species in random subsamples of size raremax from the data dataset. 
+# The result is assigned to the variable Srare. The rarefaction process involves 
+# randomly selecting a subset of individuals (or ASV sequences) from the dataset 
+# without replacement, and then calculating the number of unique species (or ASV 
+# sequences) present in each subsample. This process is repeated multiple times 
+# to estimate the expected species richness for each subsample size.
 
-library(vegan)
+Srare <- rarefy(data, raremax) # rarefy the data
 
-data(BCI)
+plot(S, Srare, xlab = "Observed No. of ASVs", ylab = "Rarefied No. of ASVs")
+abline(0, 1) # reference line
 
-BCI['Abarema.macradenia']
-
-
-S <- specnumber(BCI) # observed number of species
-(raremax <- min(rowSums(BCI)))
-Srare <- rarefy(BCI, raremax)
-plot(S, Srare, xlab = "Observed No. of Species", ylab = "Rarefied No. of Species")
-abline(0, 1)
-rarecurve(BCI, step = 20, sample = raremax, col = "blue", cex = 0.6)
+rarecurve(data, step = 20, sample = raremax, col = "blue", cex = 0.6, ylab = 'ASVs') # rarefaction curve
