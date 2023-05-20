cfb1a006dc681fac8411d4f9d64e8237676636f0
Mathias Verbeke <mathias.verbeke@student.howest.be>
2023-05-20 21:16:47 +0200

    Added two new Excel file outputs: one grouped by ASV sequence and the other grouped by ASV taxonomy. Additionally, included the sample names where the ASV occurs.

--- scripts/pipeline/Reefpipe2.R
+++ scripts/pipeline/Reefpipe2.R
@@ -19,7 +19,9 @@ ToInstall <- c(
   'xlsx',
   'dplyr',
   'progress',
-  'stringr'
+  'stringr',
+  'dplyr', 
+  'purrr'
 )
 
 for (item in ToInstall){
@@ -54,6 +56,7 @@ similarity <- args$similarity
 taxtable <- args$taxtable
 taxlevels <- gsub(' ', '', str_to_title(strsplit(args$taxlevels, ",")[[1]]))
 
+
 ####################################
 ## ABSOLUTE PATH OF MAIN WORKFLOW ##
 ####################################
@@ -85,15 +88,15 @@ if(!dir.exists(path.unified)){
   dir.create(path.unified)
 }
 
+
 ###############################################################################
 ## MERGING AND SUBSEQUENT MULTIPLE SEQUENCE ALIGNMENT OF ALL MULTIFASTA FILES##
 ###############################################################################
 
-cat('\n __  __ ____    _    
-|  \\/  / ___|  / \\   
-| |\\/| \\___ \\ / _ \\  
-| |  | |___) / ___ \\ 
-|_|  |_|____/_/   \\_\\\n\n')
+cat("\n _ __ ___  ___  __ _ 
+| '_ ` _ \\/ __|/ _` |
+| | | | | \\__ \\ (_| |
+|_| |_| |_|___/\\__,_|\n\n")
 
 result <- system2(command = 'python', args = c(file.path(dirname(pipeline_path), 'dependencies/MSA.py'), '-b', mainpath, '-o', path.unified))
 
@@ -102,15 +105,17 @@ if(result != 0){
   stop('MSA.py script execution failed.')
 }
 
+
 ##########################
 ## CLUSTERING SEQUENCES ##
 ##########################
 
-cat('\n  ____ _    _   _ 
- / ___| |  | | | |
-| |   | |  | | | |
-| |___| |__| |_| |
- \\____|_____\\___/ \n\n')
+cat("\n      _       
+     | |      
+  ___| |_   _ 
+ / __| | | | |
+| (__| | |_| |
+ \\___|_|\\__,_|\n\n")
 
 # Message
 cat('Clustering the sequences\n')
@@ -120,11 +125,6 @@ levels <- c("Phylum", "Class", "Order", "Family", "Genus", "Species")
 
 # Import aligned sequence data
 dna_seq <- readDNAStringSet(file.path(path.unified, 'AlignedCOIASVS.fasta'))
-
-
-###############################
-## CLUSTER THE ASV SEQUENCES ##
-###############################
 
 # Transform aligned sequence data
 dna_seq <- dna(as.vector(dna_seq))
@@ -136,12 +136,25 @@ threshold <- 1 - (similarity/100)
 clusters <- seq_cluster(x = dna_seq, threshold = threshold, method = "single")
 cat(paste('Clustered', length(clusters), 'ASVs into', max(clusters), 'clusters\n'))
 
+
 #############################
 ## MAIN GENERAL EXCEL FILE ##
 #############################
 
+cat("\n      _          
+     | |         
+__  _| |_____  __
+\\ \\/ / / __\\ \\/ /
+ >  <| \\__ \\>  < 
+/_/\\_\\_|___/_/\\_\\\n\n")
+
+cat(paste('Generating', file.path(path.unified, 'Union.xlsx\n')))
+
+# make new ASV IDs
+ASVID_new <- paste0("ASV", seq(1, length(as.character(dna_seq))))
+
 # Constructing data frame with ID, sequence and cluster information
-information <- data.frame(Sequence = gsub('-', '', as.character(dna_seq)), Cluster = clusters)
+information <- data.frame(ASVID = ASVID_new, Sequence = gsub('-', '', as.character(dna_seq)), Cluster = clusters)
 rownames(information) <- names(dna_seq)
 
 # Getting separate taxonomic classification tables
@@ -193,7 +206,131 @@ for(name in rownames(information)){
   }
 }
 
+# Get paths to seqtab.rds files
+seqtab_files <- list.files(mainpath, pattern = "seqtab.rds$", recursive = TRUE, full.names = TRUE)
+
+# Read all of the seqtab.rds files
+seqtab_list <- map(seqtab_files, function(x){readRDS(x)})
+
+cat('Determining samples\n')
+
+sample_names <- c()
+
+# Loop over all the information rows
+for(row in 1:nrow(information)){
+  sequence <- information[row, 'Sequence']
+  samples <- c()
+  
+  # Loop over all the sequence tables
+  for(seqtab in seqtab_list){
+    
+    # Check if the ASV sequence is present in the sequence table
+    if(sequence %in% colnames(seqtab)){
+      
+      # Loop over all the samples of the sequence table
+      for(seqtab_row in 1:nrow(seqtab)){
+        
+        # Check if the abundance of the sequence in the sample is bigger than 0
+        if(seqtab[seqtab_row, sequence] > 0){
+          samples <- c(samples, rownames(seqtab)[seqtab_row])
+          information[information$Sequence == sequence, rownames(seqtab)[seqtab_row]] <- seqtab[seqtab_row, sequence]
+          
+          if(!rownames(seqtab)[seqtab_row] %in% sample_names){
+            sample_names <- c(sample_names, rownames(seqtab)[seqtab_row])
+          }
+        }
+      }
+    }
+  }
+  
+  # Sort the samples
+  samples <- sort(samples)
+  
+  # Concatenate the samples
+  sample_string <- paste(samples, collapse = ';')
+  
+  # Add the concatenated samples to the information data frame
+  information[information$Sequence == sequence, 'Samples'] <- sample_string 
+}
+
+# Rearrange the sample columns
+sample_names <- sort(sample_names)
+
+# Put the sample columns in a separate data frame
+sample_columns <- information[, sample_names]
+
+# Remove the sample columns from the information data frame
+information <- information[,!colnames(information) %in% sample_names]
+
+# Add the sample columns (sorted) to the information data frame
+information <- data.frame(information, sample_columns)
+
 # Writing this to an Excel file
 write.xlsx(x = information, file = file.path(path.unified, 'Union.xlsx'))
 
-warnings()
+
+######################################
+## GROUP ALL ASVs BASED ON TAXONOMY ##
+######################################
+
+cat(paste('Generating', file.path(path.unified, 'GroupedTaxa.xlsx\n')))
+
+# Make an information backup
+backup_information <- information
+
+# Replace NA values with a placeholder value (e.g., "NA")
+information[is.na(information)]<- "#N/A"
+
+# Specify the formula for grouping
+formula <- as.formula(paste(". ~", paste(taxlevels, collapse = " + ")))
+
+# Join rows based on identical taxlevels
+joined <- aggregate(formula, data = information, FUN = paste, collapse = ', ')
+
+# Replace 'NA' values back to NA values
+joined[joined == '#N/A'] <- NA
+
+# Sort the rows by taxonomic level
+joined <- arrange(joined, !!!syms(taxlevels))
+
+# Add up the numbers in the sample columns
+joined[, sample_names] <- apply(joined[, sample_names], 1, function(x){
+  as.character(x)
+  
+})
+
+# Write the taxlevel grouped data frame to an Excel file
+write.xlsx(x = joined, file = file.path(path.unified, 'GroupedTaxa.xlsx'), row.names = F)
+
+
+##########################################
+## GROUP ALL ASVs BASED ON ASV SEQUENCE ##
+##########################################
+
+cat(paste('Generating', file.path(path.unified, 'GroupedSequences.xlsx\n')))
+
+# Use backup_information to override information
+information <- backup_information
+
+# Make a column that contains the concatenated string of all taxonomy columns
+information$Taxonomy <- apply(information[, taxlevels], 1, function(x){
+  paste(as.character(x), collapse = ' ')
+})
+
+# Drop columns specified in taxlevels from the data frame information
+information <- information[, !(colnames(information) %in% taxlevels)]
+
+# Replace NA values with a placeholder value (e.g., "NA")
+information[is.na(information)]<- "#N/A"
+
+# Join rows based on identical ASV sequence
+joined <- aggregate(. ~ Sequence, data = information, FUN = paste, collapse = ', ')
+
+# Replace 'NA' values back to NA values
+joined[joined == '#N/A'] <- NA
+
+# Sort the rows by ASV sequence
+joined <- arrange(joined, Sequence)
+
+# Write the sequence grouped data frame to an Excel file
+write.xlsx(x = joined, file = file.path(path.unified, 'GroupedSequences.xlsx'), row.names = F)
